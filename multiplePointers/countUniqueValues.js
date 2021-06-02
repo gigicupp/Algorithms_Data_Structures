a6552b77b63250eadabc6f5750ab390504ee312a
gigicupp <gigicupp@gmail.com>
2021-06-01 21:49:08 -0700

    solved with O(n) by altering original array

--- multiplePointers/countUniqueValues.js
+++ multiplePointers/countUniqueValues.js
@@ -2,21 +2,36 @@
 //array, and counts the unique values in the array. There can be negative
 //values but it will always be sorted.
 
+
+//without altering original array
+// function countUniqueValues(arr) {
+//   if(!arr.length) return 0;
+//   let left = 0;
+//   let right = 1;
+//   let counter = 1;
+//   while(right < arr.length) {
+//     if(arr[left] === arr[right]) {
+//       right ++;
+//     } else {
+//       left = right;
+//       right += 1;
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
+
+//alter original array
 function countUniqueValues(arr) {
   if(!arr.length) return 0;
-  let left = 0;
-  let right = 1;
-  let counter = 1;
-  while(right < arr.length) {
-    if(arr[left] === arr[right]) {
-      right ++;
-    } else {
-      left = right;
-      right += 1;
-      counter += 1;
+  let i = 0;
+  for(let j = 1; j < arr.length; j ++) {
+    if(arr[i] !== arr[j]) {
+      i ++;
+      arr[i] = arr[j];
     }
   }
-  return counter;
+  return i + 1;
 }
 
 console.log(countUniqueValues([1, 1, 1, 1, 2])) //2
