ef40f25163f4cd868a23a259276d9c4420cc92d9
gigicupp <gigicupp@gmail.com>
2021-06-11 23:34:54 -0700

    bubble sort with nested loop but with slight optimization

---
+++ sortingAlgorithms/bubbleSort_2.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  for(let i = arr.length; i > 0; i --) {
+    for(let j = 0; j < i - 1; j ++) {
+      if(arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([43,23,42,10]));
+//because bubble sort brings the highest number to the back
+//no don't have to do the comparison once those numbers are sorted
+//so if the outer loop starts from the back
+//the inner loop only runs for the unsorted part
