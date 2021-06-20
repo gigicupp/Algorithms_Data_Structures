0a2c7e9315fd6abb84bffb0f2303bdc2405e5cd7
gigicupp <gigicupp@gmail.com>
2021-06-19 20:52:52 -0700

    singly linked list unshift

---
+++ data_structures/singlyLinkedList_unshift.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length ++;
+    return this;
+  }
+
+  pop() {
+    if(!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while(current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length --;
+    if(!this.length) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if(!this.head) return undefined;
+
+    let oldHead = this.head;
+    this.head = oldHead.next;
+    this.length --;
+    if(!this.length) {
+      this.tail = null;
+    }
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length ++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+
+list.unshift(0);
+console.log(list);
