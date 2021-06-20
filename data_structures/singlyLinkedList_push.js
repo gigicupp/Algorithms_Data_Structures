d59c043927c541f173dd4a959215b3f4fcd815a7
gigicupp <gigicupp@gmail.com>
2021-06-19 19:39:25 -0700

    singly linked list push

--- data_structures/singlyLinkedList_push.js
+++ data_structures/singlyLinkedList_push.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -12,10 +19,23 @@ class SinglyLinkedList {
     if(!this.head) {
       //set the head and the tail to be the newly created node.
       this.head = node;
+      this.tail = this.head;
+    } else {
+      //otherwise set the next prop on the existing tail to be the new node
+      this.tail.next = node;
+      //make the new node to be the new tail
       this.tail = node;
     }
-    //otherwise set the next prop on the tail to be the new node
-    this.tail.next = node;
-    this.length += 1;
+    //increment the size of the list
+    this.length ++;
+    //return the list
+    return this;
   }
 }
+
+let test = new SinglyLinkedList();
+console.log(test);
+
+test.push('Hello');
+test.push('my list');
+console.log(test);
